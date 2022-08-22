405d2ba05da4606f2d066de518142a65321c8910
Anderson Alves <nenoalveslopes@hotmail.com>
2022-08-22 13:03:10 -0300

    Projeto finalizado futuramente pretensão de adicionar cardápio

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+
+const menu = document.querySelector('.menu');
+const closedMenu = document.querySelector('.closed-menu');
+const aside = document.querySelector('aside');
+const body = document.querySelector('body');
+const openModal = document.querySelector('#openModal');
+const closedModal = document.querySelector('#closedModal');
+const modalBox = document.querySelector('.reservas-modal-box');
+menu.addEventListener('click',function(){
+    aside.style.width = '100%';
+    body.style.overflow = 'clip';
+});
+closedMenu.addEventListener('click',function(){
+    aside.style.width = '0';
+    body.style.overflow = 'initial';
+});
+openModal.addEventListener('click',function(){
+    openModal.style.opacity = '0';
+    modalBox.style.width = '40vw';
+    setTimeout(function(){
+        modalBox.style.opacity = '1';
+        openModal.style.display = 'none';
+    },600);
+    
+});
+closedModal.addEventListener('click',function(){
+    openModal.style.opacity = '1';
+    modalBox.style.opacity = '0'
+    setTimeout(function(){
+        modalBox.style.width = '0';
+        openModal.style.display = 'block';
+    },500)
+    
+    
+});
